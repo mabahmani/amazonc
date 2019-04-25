61da37fec523c027a33b3379f03751d01981e9e2
mab <aminbahmani10@gmail.com>
2019-04-25 11:01:35 +0430

    add Api

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 var Crawler = require("crawler");
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', function (req, res){
 let reviews = [];
 
-var c = new Crawler({
-    maxConnections: 10,
-    callback: function (error, res, done) {
-        if (error) {
-            console.log(error);
-        } else {
-            var $ = res.$;
+var page = 'https://www.amazon.com/product-reviews/{{isbn}}/ref=cm_cr_arp_d_viewopt_srt?reviewerType=all_reviews&pageNumber={{page}}'
 
+page = page.replace('{{isbn}}',req.query.isbn);
+page = page.replace('{{page}}',req.query.page);
+
+var c = new Crawler({ maxConnections : 10 });
+
+function crawlerPromise(options) {
+  return new Promise((resolve, reject) => {
+    options.callback = (err, res, done) => {
+      if (err) {
+        reject(err);
+      } else {
+        var $ = res.$;
+            console.log(page);
             $('.review').each(function(index) {
                 let review = {
-                    name: $('.a-profile-name', this).text().trim(),
-                    rate: $('.a-icon-alt', this).text().trim(),
+                    name: $('.a-profile-namlt', this).text().trim(),
                     title: $('.review-title', this).text().trim(),
+                    rate: $('.a-icon-ae', this).text().trim(),
                     date: $('.review-date', this).text().trim(),
                     text: $('.review-text', this).text().trim()
-                };
-                //reviews.push(review);
-                console.log(JSON.stringify(review));
-            });
-        }
-        done();
+                    };
+                    reviews.push(review);
+                });
+        resolve(reviews);
+      }
+      done();
     }
+    c.queue(options);
+  });
+}
+
+crawlerPromise({ uri: page })
+    .then((reviews) => {res.json(reviews)})
+    .catch((error) => {console.log(error)});
 });
 
-c.queue('https://www.amazon.com/product-reviews/B073YTMMQS/ref=cm_cr_arp_d_viewopt_srt?reviewerType=all_reviews&pageNumber=0');
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
