783c0bf0991490986e85d4b7b4dae43af5a49aea
mab <aminbahmani10@gmail.com>
2019-04-25 13:24:09 +0430

    add GoodReads Crawler

--- app.js
+++ app.js
@@ -44,4 +44,43 @@ crawlerPromise({ uri: page })
     .catch((error) => {console.log(error)});
 });
 
+app.get('/goodreads', function (req, res){
+  let reviews = [];
+  
+  var page = 'https://www.goodreads.com/api/reviews_widget_iframe?isbn={{isbn}}&page={{page}}'
+  
+  page = page.replace('{{isbn}}',req.query.isbn);
+  page = page.replace('{{page}}',req.query.page);
+  
+  var c = new Crawler({ maxConnections : 10 });
+  
+  function crawlerPromise(options) {
+    return new Promise((resolve, reject) => {
+      options.callback = (err, res, done) => {
+        if (err) {
+          reject(err);
+        } else {
+          var $ = res.$;
+              console.log(page);
+              $('.gr_review_container').each(function(index) {
+                  let review = {
+                      name: $('.gr_review_by', this).text().trim().replace("By ",""),
+                      date: $('.gr_review_date', this).text().trim(),
+                      text: $('.gr_review_text', this).text().trim().replace("...more",""),
+                      };
+                      reviews.push(review);
+                  });
+          resolve(reviews);
+        }
+        done();
+      }
+      c.queue(options);
+    });
+  }
+  
+  crawlerPromise({ uri: page })
+      .then((reviews) => {res.json(reviews)})
+      .catch((error) => {console.log(error)});
+  });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
